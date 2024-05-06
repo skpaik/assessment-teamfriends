cd142130711fe1e40ba6cfdd224bad40964bb7fe
Sudipta K Paik <sdiptapaik@gmail.com>
2024-05-06 14:08:38 +0600

    Delete migrate_db.sh

--- migrate_db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python3 manage.py makemigrations
-python3 manage.py migrate
-
-python3 manage.py syncdb --noinput
-#echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
-
